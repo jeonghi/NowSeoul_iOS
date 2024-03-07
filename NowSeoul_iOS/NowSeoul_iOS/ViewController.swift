9a0957e22402daf6eba370b9036bf46ad8a1333a
jeonghi <jpark0902@kookmin.ac.kr>
2024-03-07 14:40:15 +0900

    [init] 프로젝트 초기 설정

---
+++ NowSeoul_iOS/NowSeoul_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NowSeoul_iOS
+//
+//  Created by 쩡화니 on 3/7/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
